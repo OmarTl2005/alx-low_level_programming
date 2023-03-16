768660ba7f1328f0c3a504c0fe43b3d29f743a1f
OmarTl2005 <hiba41145@gmail.com>
2023-03-16 18:27:37 +0100

    new projects

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-*main - entery poit
+*main - entery point
 *return: always 0 (success)
 */
 int main(void)
 {
         puts("\"Programming is like building a multilingual puzzle");
-        return 0;
+        return (0);
 }
