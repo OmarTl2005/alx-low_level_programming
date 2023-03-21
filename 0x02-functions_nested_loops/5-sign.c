35444a291aa1dff7f7fe772248b43852d4c3d8fb
OmarTl2005 <hiba41145@gmail.com>
2023-03-21 21:35:55 +0000

    additions

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,16 +12,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		return (1);
-		_putchar('+');
+		_putchar(43);
 	}
 	else if (n == 0)
 	{
 		return (0);
-		_putchar('0');
+		_putchar(48);
 	}
 	else
 	{
 		return (-1);
-		_putchar('-');
+		_putchar(45);
 	}
 }
