41ac570fe3eb6d9d8b7fa268d30e06db781359b5
OmarTl2005 <hiba41145@gmail.com>
2023-03-21 22:27:30 +0000

    additions

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,12 +11,14 @@ void jack_bauer(void)
 
 	for (a = 0; a <= 2; a++)
 	{
-		for (b = 0; b <= 3; b++)
+		for (b = 0; b <= 9; b++)
 		{
 			for (c = 0; c <= 5; c++)
 			{
 				for (d = 0; d <= 9; d++)
 				{
+					if (a >= 2 && b >= 4)
+						break;
 					_putchar(a + 48);
 					_putchar(b + 48);
 					_putchar(58);
