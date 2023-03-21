3583f27f67a6684d3b25d2a905e206fe02348d6b
OmarTl2005 <hiba41145@gmail.com>
2023-03-21 22:15:43 +0000

    additions

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+  *jack_bauer - Prints every minute of the day, from 00:00 to 23:59
+  *
+  *Return: always 0 (Success)
+  */
+
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a <= 2; a++)
+	{
+		for (b = 0; b <= 3; b++)
+		{
+			for (c = 0; c <= 5; c++)
+			{
+				for (d = 0; d <= 9; d++)
+				{
+					_putchar(a);
+					_putchar(b);
+					_putchar(58);
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
