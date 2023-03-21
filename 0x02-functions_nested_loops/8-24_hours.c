cb30895d8f9c89950f6904a5df9d793c9f7bedaa
OmarTl2005 <hiba41145@gmail.com>
2023-03-21 22:18:53 +0000

    additions

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,6 +17,11 @@ void jack_bauer(void)
 			{
 				for (d = 0; d <= 9; d++)
 				{
+					if (a >= 2 && b >= 4)
+						{
+							break;
+						}
+
 					_putchar(a);
 					_putchar(b);
 					_putchar(58);
