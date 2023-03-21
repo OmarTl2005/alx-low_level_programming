9a4791460ac1b44470fd3c44ff2a285045541228
OmarTl2005 <hiba41145@gmail.com>
2023-03-21 15:16:06 +0000

    additions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,15 +5,12 @@
   *Return: always 0
   */
 
-int main(void)
+void print_alphabet(void)
 {
-	void print_alphabet(void)
+	int i;
+	for (i = 97; i <=122; i++)
 	{
-		int i;
-		for (i = 97; i <=122; i++)
-		{
-			_putchar(i);
-		}
-		_putchar('\n');
+		_putchar(i);
 	}
+	_putchar('\n');
 }
