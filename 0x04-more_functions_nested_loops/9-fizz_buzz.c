e268c8ccea6ad46cd710f06463202e6a3c0a7e88
OmarTl2005 <hiba41145@gmail.com>
2023-03-23 21:00:20 +0000

    additions

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,7 +17,14 @@ int main(void)
 		}
 		else if (num % 5 == 0 && num % 3 != 0)
 		{
+			if (num < 100)
+			{
 			printf("Buzz ");
+			}
+			else
+			{
+				printf("Buzz");
+			}
 		}
 		else if (num % 3 == 0 && num % 5 == 0)
 		{
