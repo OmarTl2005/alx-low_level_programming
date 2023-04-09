23ebc86dc5bb9495ef070181df4884bb75dbb956
OmarTl2005 <hiba41145@gmail.com>
2023-04-09 17:55:17 +0000

    additions

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+  *main - Entry point
+  *@argc: argument counter
+  *@argv: argument vector
+  *Return: always 0
+  */
+
+int main(int argc, char *argv[])
+{
+	int i, j, add = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[i]);
+	}
+	printf("%d\n", add);
+	return (0);
+}
