bcdfcb7344a7e7214a18630461adfce181e5627f
OmarTl2005 <hiba41145@gmail.com>
2023-04-18 01:13:52 +0000

    additions

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - free allocated memory
+ * @d: struct pointer
+ *
+ * Return: void.
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+
+}
