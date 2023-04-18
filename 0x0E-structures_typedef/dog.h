346d3a5161da3d01c0b24178751ec3ceba6f0ae9
OmarTl2005 <hiba41145@gmail.com>
2023-04-18 00:23:33 +0000

    additions

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  *struct dog - structures about a dog informations
+  *@name: name of the dog
+  *@owner: name of the owner
+  *@age: age
+  *Description: structures about a dog informations.
+  */
+
+struct dog
+{
+	char *name , *owner;
+	float age;
+};
