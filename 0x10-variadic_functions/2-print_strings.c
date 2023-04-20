542a05463b67a33ab549210723bb054feed2f1c1
OmarTl2005 <hiba41145@gmail.com>
2023-04-20 18:00:19 +0000

    additions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		printf("%s", s);
 		if (s == NULL)
-			printf("(nil)");
+			printf("%s", "(nil)");
 		if (separator && i < n - 1)
 			printf("%s", separator);
 	}
