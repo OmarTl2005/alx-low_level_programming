f3603ea8be6d6b08bdeadfa20166468d8a131f2f
OmarTl2005 <hiba41145@gmail.com>
2023-04-20 18:05:06 +0000

    additions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,25 +1,24 @@
-#include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
+#include "variadic_functions.h"
 /**
-  *print_strings - prints strs
-  *@separator: separator
-  *@n: num of par
-  *Return: strings
-  */
+ * print_strings-  prints all the enteries in of the function.
+ * @n: number of elements to be added
+ * @separator: character between numbers
+ * Return: Returns nothing.
+ */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i = 0;
+	unsigned int i;
 	va_list ap;
 	char *s;
 
 	va_start(ap, n);
-
 	for (i = 0; i < n; i++)
 	{
 		s = va_arg(ap, char *);
-
-		printf("%s", s);
+		if (s)
+			printf("%s", s);
 		if (s == NULL)
 			printf("%s", "(nil)");
 		if (separator && i < n - 1)
