7ff1973064f5ab7002d0ff95d5c6877d9c8d2fc4
OmarTl2005 <hiba41145@gmail.com>
2023-06-16 00:41:07 +0100

    fixed

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+  *free_dlistint - frees a dlistint_t list.
+  *@head: the fiirst node
+  *Return: nothing
+  */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *node;
+
+	while (head != NULL)
+	{
+		node = head;
+		head = head->next;
+		free(node);
+	}
+}
