cec2f880fc4c1a602795b76c6647d3bc679482e3
OmarTl2005 <hiba41145@gmail.com>
2023-06-16 02:17:23 +0100

    fixed

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,17 +1,20 @@
 #include <stdlib.h>
 #include "lists.h"
-
+/**
+  *sum_dlistint - something
+  *@head: the head node
+  *Return: sum
+  */
 int sum_dlistint(dlistint_t *head)
 {
-    int sum = 0;
-    dlistint_t *current;
+	int sum = 0;
+	dlistint_t *current;
 
-    current = head;
-    while (current != NULL) {
-        sum += current->n;
-        current = current->next;
-    }
-
-    return sum;
+	current = head;
+	while (current != NULL)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+	return (sum);
 }
-
